9bbaac4edbc1712d8df42a67b62582868e275f05
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2022-11-19 13:15:20 +0100

    Login, register forms

--- Shoppie/Models/User.cs
+++ Shoppie/Models/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public Address? Address { get; set; }
+        public string PasswordHash { get; set; }
     }
 }
