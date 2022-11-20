eebca4275c95460906cc4dd37e932abce1abe055
Fipek <filip.bialowas2504@gmail.com>
2022-11-20 12:36:22 +0100

    Added form for product

---
+++ Shoppie/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shoppie.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(IFormCollection Form)
+        {
+            return View();
+        }
+    }
+}
