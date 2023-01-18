b894baf6c024140149cf5f775fa796ff49757663
slik margor <slikmargor@gmail.com>
2023-01-18 10:45:10 +0100

    Scaffolded The identity and moved the Context + User to the DataAccess Assembly. Added Globla using and removed those unnecessary

---
+++ Data/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shoppie.DataAccess;
+
+// Add profile data for application users by adding properties to the AppUser class
+public class AppUser : IdentityUser
+{
+}
+
---
+++ Shoppie/GlobalUsings/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using System.Text;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Microsoft.AspNetCore.WebUtilities;
+global using Shoppie.DataAccess;
+global using System.ComponentModel.DataAnnotations;
+global using System.Text.Encodings.Web;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Identity.UI.Services;
