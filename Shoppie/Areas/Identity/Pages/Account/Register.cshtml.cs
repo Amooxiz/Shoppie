3d3d249bb6c2e359b6a72636a8108b719ff6a8f0
slik margor <slikmargor@gmail.com>
2023-01-18 10:50:37 +0100

    Created the Database with migrations

--- Shoppie/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Shoppie/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,12 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity.UI.Services;
-
-
 namespace Shoppie.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
