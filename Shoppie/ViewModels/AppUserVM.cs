48f7cf9b89b975c7b85f626df1c756d6e46a8435
slik margor <slikmargor@gmail.com>
2023-01-24 11:07:59 +0100

    Correcting missing data in AppUserVM

--- Shoppie/ViewModels/AppUserVM.cs
+++ Shoppie/ViewModels/AppUserVM.cs
@@ -2,5 +2,17 @@
 {
     public class AppUserVM
     {
+        public string Id { get; set; }
+        public string Street { get; set; }
+        public int? ApartamentNr { get; set; }
+        public string BuildingNr { get;  set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public double PersonalDicount { get; set; }
+        public string PostalCode { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
     }
 }
