1c878d11525ffc33d0f94f2867c001722b919f81
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2023-01-26 16:57:47 +0100

    Cart model

---
+++ Shoppie/SupportModels/Cart.cs
@@ -0,0 +1,10 @@
+﻿using Shoppie.ViewModels;
+
+namespace Shoppie.SupportModels
+{
+    public class Cart
+    {
+        public int ItemCount { get; set; }
+        public List<OfferVM> Items { get; set; }
+    }
+}
