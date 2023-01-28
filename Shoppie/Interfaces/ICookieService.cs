28697502e11faa17a1e57c966bced390ff401483
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2023-01-28 04:13:03 +0100

    Cookie + NBP services

---
+++ Shoppie/Interfaces/ICookieService.cs
@@ -0,0 +1,16 @@
+﻿using Shoppie.SupportModels;
+using Shoppie.ViewModels;
+
+namespace Shoppie.Interfaces
+{
+    public interface ICookieService
+    {
+        string GetCookie(string key);
+        void RemoveCookie(string key);
+        void SetCookie(string key, string value);
+        bool RemoveItemFromCart(int nr);
+        void AddItemToCart(OfferVM offer);
+        CartProduct MapOfferToCartProduct(OfferVM offer);
+        Cart GetCart();
+    }
+}
