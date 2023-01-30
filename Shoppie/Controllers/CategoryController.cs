df33cd3618d59903999ec1879fb66849c1322166
slik margor <slikmargor@gmail.com>
2023-01-30 10:06:00 +0100

    Createde Category Controller and GET, POST method

---
+++ Shoppie/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shoppie.Interfaces;
+using Shoppie.Services;
+
+namespace Shoppie.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ILogger<HomeController> logger, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Management()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Management(CategoryVM category)
+        {
+            return View();
+        }
+    }
+}
