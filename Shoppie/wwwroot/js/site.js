c5c3c7fa99ae6d7376692de5a8994c013d300f07
Fipek <filip.bialowas2504@gmail.com>
2023-01-31 01:31:12 +0100

    frontend

--- Shoppie/wwwroot/js/site.js
+++ Shoppie/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+const currButton = document.getElementById('currency');
+const currencies = document.querySelectorAll('#curr');
+console.log(currencies);
+
+const changeVisiblity = (value) => {
+    currencies.forEach((curr) => {
+        if (curr.style.display === "none") {
+            curr.style.display = 'block';
+        }
+        else {
+            curr.style.display = 'none';
+        }
+    })
+}
+currButton.addEventListener('click', changeVisiblity);
