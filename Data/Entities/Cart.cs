143402bf4e46e626be0c71010968fa564c0ffb13
slik margor <slikmargor@gmail.com>
2023-02-22 11:59:42 +0100

    Added Cart Icon to the main layout as well as required Db classes

---
+++ Data/Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace Shoppie.DataAccess.Entities
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public ICollection<CartItem> Items { get; set; }
+    }
+}
---
+++ Data/Entities/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppie.DataAccess.Entities
+{
+    public class CartItem
+    {
+        public int ItemId { get; set; }
+        public Offer Offer { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
